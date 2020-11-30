e395122f621ebc1635e8dfb2682e91e8a3bc7a81
dosSantos-Paulo <paulo.dossantos@hotmail.com>
2020-11-30 17:30:58 -0300

    validaçao de entradas de signupFragment

--- app/src/main/java/com/example/marvel/login/view/SignupFragment.kt
+++ app/src/main/java/com/example/marvel/login/view/SignupFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.example.marvel.R
+import com.google.android.material.textfield.TextInputLayout
 
 
 class SignupFragment : Fragment() {
@@ -24,9 +28,42 @@ class SignupFragment : Fragment() {
 
         val navController = Navigation.findNavController(view)
         val arrowButton = view.findViewById<ImageView>(R.id.arrowBack_signup)
+        val saveButton = view.findViewById<Button>(R.id.btn_save_signup)
+
+        val name = view.findViewById<TextInputLayout>(R.id.editText_name_signup).editText
+        val email = view.findViewById<TextInputLayout>(R.id.editText_email_signup).editText
+        val password = view.findViewById<TextInputLayout>(R.id.editText_password_signup).editText
+
+        saveButton.setOnClickListener {
+            nullValidation(name, email, password)
+
+            if (!name?.text?.trim().isNullOrEmpty() &&
+                !email?.text?.trim().isNullOrEmpty() &&
+                !password?.text?.trim().isNullOrEmpty()){
+                Toast.makeText(view.context, "clicou --> ${saveButton.text}", Toast.LENGTH_SHORT).show()
+            }
+        }
 
         arrowButton.setOnClickListener {
             navController.popBackStack()
+        }
+    }
+
+    private fun nullValidation(
+        name: EditText?,
+        email: EditText?,
+        password: EditText?
+    ) {
+        if (name?.text?.trim().isNullOrEmpty()) {
+            name?.error = getString(R.string.need_a_name)
+        }
+
+        if (email?.text?.trim().isNullOrEmpty()) {
+            email?.error = getString(R.string.need_an_email)
+        }
+
+        if (password?.text?.trim().isNullOrEmpty()) {
+            password?.error = getString(R.string.need_a_password)
         }
     }
 
